6e2394765ceb6c9361a4e0ef6a48fae79b30813e
Jakub Piksa <kuba.piksa@wp.pl>
2023-10-08 19:50:23 +0200

    section 5

--- js/imgChanger.js
+++ js/imgChanger.js
@@ -5,7 +5,7 @@ window.addEventListener('resize', () => {
     if (window.innerWidth > 1200) {
       image.src = 'utils/images/comp_lady-side.png';
     } else {
-console.log('Mobile view')
+      image.src = 'utils/images/comp_lady_hd.png', console.log('Mobile view')
     }
   });
 });
